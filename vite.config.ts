82d618c84ae3f5ca01188cfe93a1436144a16416
ardit-hyseni <ardit.hys@outlook.com>
2026-02-09 21:38:49 +0100

    Update Vite configuration to change base path from "/rsvp-speed-reading/" to "./" for improved deployment flexibility.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/rsvp-speed-reading/",
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
